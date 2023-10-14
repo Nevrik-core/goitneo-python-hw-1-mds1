f687deb22d358a688f617a0bd67749dd718c7c00
Nevrik-core <57769617+Nevrik-core@users.noreply.github.com>
2023-10-14 11:07:30 +0200

    task 1-1 done

--- main.py
+++ main.py
@@ -17,11 +17,16 @@ def get_birthdays_per_week(users):
         if birthday_this_year < today:
             birthday_this_year = birthday.replace(year=today.year + 1)
         delta_days = (birthday_this_year - today).days
-        print('delta_days: ', delta_days)
+        # print('delta_days: ', delta_days)
+        if delta_days < 7:
+            day_of_week = (today + timedelta(days=delta_days)).strftime('%A')
+            # print('day_of_week: ', day_of_week)
+            if day_of_week == "Saturday" or day_of_week == "Sunday":
+                day_of_week = "Monday"
+            birthdays[day_of_week].append(name)
+    for day, names in birthdays.items():
+        print(f"{day}: {', '.join(names)}")
     # print("birthdays: ", birthdays)
-    
-
-
 
 
 
